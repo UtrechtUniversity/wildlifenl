426ed5d7bc9273984dccc49492aa2d8709917c51
BasMichielsen <b.s.h.t.michielsen@uu.nl>
2024-06-01 09:01:43 +0200

    cleanup

--- species.go
+++
@@ -1,6 +0,0 @@
-package wildlifenl
-
-type Species struct {
-	Name       string `json:"name" example:"Canis familiaris" doc:"The latin binomen"`
-	CommonName string `json:"commonName" example:"Dog" doc:"The common name"`
-}
