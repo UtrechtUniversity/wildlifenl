8b44d4b78fe57b300f26454db4fa531c544b67d1
BasMichielsen <b.s.h.t.michielsen@uu.nl>
2024-09-03 09:57:31 +0200

    add roles

--- database/202409020002_role.up.sql
+++ database/202409020002_role.up.sql
@@ -3,3 +3,11 @@ CREATE TABLE IF NOT EXISTS "role" (
 	"name" VARCHAR NOT NULL,
 	PRIMARY KEY ("id")
 );
+
+INSERT INTO "role"("name") VALUES ('administrator');
+INSERT INTO "role"("name") VALUES ('data-system');
+INSERT INTO "role"("name") VALUES ('researcher');
+INSERT INTO "role"("name") VALUES ('land-user');
+INSERT INTO "role"("name") VALUES ('nature-area-manager');
+INSERT INTO "role"("name") VALUES ('wildlife-manager');
+INSERT INTO "role"("name") VALUES ('herd-manager');
