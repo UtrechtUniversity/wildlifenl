a6f5110f7b118504a3a1f7da9dac85b16f1f02a8
BasMichielsen <b.s.h.t.michielsen@uu.nl>
2024-09-10 14:40:59 +0200

    removed doc examples

--- models/animal.go
+++ models/animal.go
@@ -4,7 +4,7 @@ import "time"
 
 type Animal struct {
 	ID                string     `json:"ID" format:"uuid" readOnly:"true" doc:"The ID of this animal."`
-	Name              string     `json:"name" doc:"The name of this animal." example:"Flupke"`
+	Name              string     `json:"name" doc:"The name of this animal."`
 	Species           Species    `json:"species" doc:"The species of this animal"`
 	Location          *Point     `json:"location,omitempty" readOnly:"true" doc:"The location of this animal"`
 	LocationTimestamp *time.Time `json:"location_timestamp,omitempty" format:"datetime" readOnly:"true" doc:"The moment that this animal's location was last updated."`
--- models/species.go
+++ models/species.go
@@ -2,7 +2,7 @@ package models
 
 type Species struct {
 	ID           string `json:"ID" format:"uuid" readOnly:"true" doc:"The ID of this species."`
-	Name         string `json:"name" doc:"The Latin binomen" example:"Canis familiaris"`
-	CommonNameNL string `json:"commonNameNL" doc:"The Dutch common name" example:"Hond" `
-	CommonNameEN string `json:"commonNameEN" doc:"The English common name" example:"Dog" `
+	Name         string `json:"name" doc:"The Latin binomen"`
+	CommonNameNL string `json:"commonNameNL" doc:"The Dutch common name"`
+	CommonNameEN string `json:"commonNameEN" doc:"The English common name"`
 }
