04c27d0585b48aabbc1f3d127e5ddc42c5711ff9
BasMichielsen <b.s.h.t.michielsen@uu.nl>
2024-10-15 14:15:18 +0200

    Fixes #27

--- serveMux.go
+++ serveMux.go
@@ -12,6 +12,7 @@ func NewServeMux() *ServeMux {
 
 func (c *ServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	if r.Method == http.MethodOptions {
 		return
