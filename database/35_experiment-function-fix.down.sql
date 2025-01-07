6d83892fc13eb038065efd3c5b113b1e3bc2f1ac
BasMichielsen <b.s.h.t.michielsen@uu.nl>
2025-01-07 13:54:41 +0100

    Fixes #77

---
+++ database/35_experiment-function-fix.down.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE FUNCTION check_update_constraints()
+RETURNS TRIGGER AS '
+BEGIN
+    IF OLD."start" IS NOT NULL AND OLD."end" IS NOT NULL THEN
+        RAISE EXCEPTION ''cannot update a finished experiment'';
+    END IF;
+
+    IF NEW."start" IS DISTINCT FROM OLD."start" AND NEW."end" IS DISTINCT FROM OLD."end" THEN
+        IF OLD."start" <= NOW() THEN
+            RAISE EXCEPTION ''cannot update a started experiment 1'';
+        END IF;
+
+        IF NEW."start" <= NOW() THEN
+            RAISE EXCEPTION ''start must be later than now'';
+        END IF;
+
+        IF NEW."end" <= NEW."start" THEN
+            RAISE EXCEPTION ''end must be later than start'';
+        END IF;
+
+    ELSIF NEW."start" IS DISTINCT FROM OLD."start" THEN
+        IF OLD."start" <= NOW() THEN
+            RAISE EXCEPTION ''cannot update a started experiment 2'';
+        END IF;
+
+        IF NEW."start" <= NOW() THEN
+            RAISE EXCEPTION ''start must be later than now'';
+        END IF;
+
+        IF OLD."end" IS NOT NULL AND NEW."start" >= OLD."end" THEN
+            RAISE EXCEPTION ''start must be earlier than end'';
+        END IF;
+
+    ELSIF NEW."end" IS DISTINCT FROM OLD."end" THEN
+        IF NEW."end" <= OLD."start" THEN
+            RAISE EXCEPTION ''end must be later than start'';
+        END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+' LANGUAGE plpgsql;
