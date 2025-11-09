960ac58b55e2a484a44437e328f5aa7c5f7a08dd
BasMichielsen <b.s.h.t.michielsen@uu.nl>
2025-11-09 09:45:16 +0100

    Fixes #137

--- models/AnimalInfo.go
+++ models/AnimalInfo.go
@@ -1,7 +1,7 @@
 package models
 
 type AnimalInfo struct {
-	Sex       string `json:"sex" enum:"female,male,other" doc:"The sex of the observed animal."`
+	Sex       string `json:"sex" enum:"female,male,unknown" doc:"The sex of the observed animal."`
 	LifeStage string `json:"lifeStage" enum:"infant,adolescent,adult,unknown" doc:"The life stage of the observed animal."`
 	Condition string `json:"condition" enum:"healthy,impaired,dead,other" doc:"The condition of the observed animal."`
 }
