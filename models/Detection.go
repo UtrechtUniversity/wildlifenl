4ea1cc7c953708a1607b86a57e40e378d38adf3a
BasMichielsen <b.s.h.t.michielsen@uu.nl>
2026-01-29 08:36:23 +0100

    fixed docs

--- models/Detection.go
+++ models/Detection.go
@@ -6,11 +6,11 @@ import (
 
 type DetectionRecord struct {
 	ID         string            `json:"ID" format:"uuid" readOnly:"true" doc:"The ID of this detection."`
-	SpeciesID  string            `json:"speciesID" format:"uuid" writeOnly:"true" required:"true" doc:"The ID of the species of the detected animal."`
-	SensorID   string            `json:"sensorID" minLength:"2" doc:"The identification for the sensor that detected the animal."`
-	SensorType *string           `json:"sensorType" enum:"visual,acoustic,motion,radio,chemical,other" doc:"The type of the sensor that detected the animal."`
-	Location   Point             `json:"location" doc:"The location that the animal was detected at."`
-	Timestamp  time.Time         `json:"timestamp" format:"date-time" doc:"The moment the animal was detected."`
+	SpeciesID  string            `json:"speciesID" format:"uuid" writeOnly:"true" required:"true" doc:"The ID of the species of the detected animal(s)."`
+	SensorID   string            `json:"sensorID" minLength:"2" doc:"The identification for the sensor that made this detection."`
+	SensorType *string           `json:"sensorType" enum:"visual,acoustic,motion,radio,chemical,other" doc:"The type of the sensor that made this detection."`
+	Location   Point             `json:"location" doc:"The location that the detection happened at."`
+	Timestamp  time.Time         `json:"timestamp" format:"date-time" doc:"The moment that the detection happened at."`
 	URI        *string           `json:"uri,omitempty" format:"uri" doc:"The URI to view this detection in a publicly available external system."`
 	Animals    []DetectionAnimal `json:"animals" doc:"Information on the animals involved in this detection."`
 }
