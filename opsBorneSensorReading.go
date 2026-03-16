07dffb04d72149ea50be7ca7da58b177d628d7f7
BasMichielsen <b.s.h.t.michielsen@uu.nl>
2026-03-16 11:06:34 +0100

    fix animal nil check

--- opsBorneSensorReading.go
+++ opsBorneSensorReading.go
@@ -62,17 +62,15 @@ func (o *borneSensorReadingOperations) RegisterAdd(api huma.API) {
 		if err != nil {
 			return nil, handleError(err)
 		}
-		if time.Since(*animal.LocationTimestamp) < time.Hour {
+		if animal != nil && time.Since(*animal.LocationTimestamp) > time.Hour {
 			// Add Borne-Sensor-Reading -> Create Alarms.
-			if animal != nil {
-				ids, err := stores.NewAlarmStore(relationalDB).AddAllFromAnimal(animal)
-				if err != nil {
-					return nil, handleError(err)
-				}
-				// From created Alarms -> Create Conveyances
-				if err := stores.NewConveyanceStore(relationalDB).AddForAlarmIDs(ids); err != nil {
-					return nil, handleError(err)
-				}
+			ids, err := stores.NewAlarmStore(relationalDB).AddAllFromAnimal(animal)
+			if err != nil {
+				return nil, handleError(err)
+			}
+			// From created Alarms -> Create Conveyances
+			if err := stores.NewConveyanceStore(relationalDB).AddForAlarmIDs(ids); err != nil {
+				return nil, handleError(err)
 			}
 		}
 		return nil, nil
